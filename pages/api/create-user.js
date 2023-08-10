c6a670393e4b6683d7eeeb744dc193a2991558a9
Shalabh Kamboj <71832334+kambojshalabh35@users.noreply.github.com>
2023-08-10 22:02:05 +0530

    1

--- pages/api/create-user.js
+++ pages/api/create-user.js
@@ -26,5 +26,5 @@ export default async function handler(req, res) {
 
         res.status(200).json(data)
     }
+    res.status(200).json({Hello:"Hello World"})
 }
-// Lfyx39pVQ6oHJUCF
--- pages/api/login-user.js
+++ pages/api/login-user.js
@@ -30,5 +30,5 @@ export default async function handler(req, res) {
         }
 
     }
+    res.status(200).json({Hello:"Hello World"})
 }
-// Lfyx39pVQ6oHJUCF
